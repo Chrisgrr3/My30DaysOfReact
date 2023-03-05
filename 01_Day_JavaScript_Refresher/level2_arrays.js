5d68bae5651960e9043054957849f9fcfe9e5c5b
Christopher Guerrero <chrisgrr3@gmail.com>
2023-03-04 23:51:26 -0700

    Imported countries and webTechs arrays

--- 01_Day_JavaScript_Refresher/level2_arrays.js
+++ 01_Day_JavaScript_Refresher/level2_arrays.js
@@ -1,3 +1,6 @@
+const webTechs = require('./web_techs')
+const countries = require('./countries')
+
 // 1. Create a separate countries.js file and store the countries array into this file, create a separate file web_techs.js and store the webTechs array into this file. Access both file in main.js file
 
 
