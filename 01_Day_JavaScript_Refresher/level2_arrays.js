4c20627b1bfbcc7ddfb76debcb4ad492a3dd629a
Christopher Guerrero <chrisgrr3@gmail.com>
2023-03-04 23:57:04 -0700

    Removed commas from text

--- 01_Day_JavaScript_Refresher/level2_arrays.js
+++ 01_Day_JavaScript_Refresher/level2_arrays.js
@@ -4,9 +4,13 @@ const countries = require('./countries')
 // 1. Create a separate countries.js file and store the countries array into this file, create a separate file web_techs.js and store the webTechs array into this file. Access both file in main.js file
 
 
-// 2. First remove all the punctuations and change the string to array and count the number of words in the array
+// 2. First remove all the punctuation,s and change the string to array and count the number of words in the array
 let text =
   'I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.'
+
+text = text.replace(/,/g, '')
+
+console.log(text)
 
 
 // 3. In the following shopping cart add, remove, edit items
