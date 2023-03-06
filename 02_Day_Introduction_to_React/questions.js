ec46b3021695618342346713a0d1cd168c4cfec8
Christopher Guerrero <chrisgrr3@gmail.com>
2023-03-06 11:47:37 -0700

    Formatting

--- 02_Day_Introduction_to_React/questions.js
+++ 02_Day_Introduction_to_React/questions.js
@@ -1,33 +1,64 @@
-Exercises: What is React?
-What is React?
-What is a library?
-What is a single page application?
-What is a component ?
-What is the latest version of React?
-What is DOM?
-What is React Virtual DOM?
-What does a web application or a website(composed of) have?
-Exercises: Why React?
-Why did you chose to use react?
-What measures do you use to know popularity ?
-What is more popular, React or Vue ?
-Exercises: JSX
-What is an HTML element?
-How to write a self closing HTML element?
-What is an HTML attribute? Write some of them
-What is JSX?
-What is babel?
-What is a transpiler?
-Exercises: JSX Elements
-What is a JSX element?
-Write your name in a JSX element and store it in a name variable
-Write a JSX element which displays your full name, country, title, gender, email, phone number. Use h1 for the name and p for the rest of the information and store it in a user variable
-Write a footer JSX element
-Exercises: Inline Style
-Create a style object for the main JSX
-Create a style object for the footer and app JSX
-Add more styles to the JSX elements
-Exercises: Internal Styles
-Apply different styles to your JSX elements
-Exercise: Inject data to JSX
-Practice how to make JSX element and injecting dynamic data(string, number, boolean, array, object)
+// Exercises: What is React?
+//     1. What is React?
+    
+//     2. What is a library?
+    
+//     3. What is a single page application?
+    
+//     4. What is a component ?
+    
+//     5. What is the latest version of React?
+    
+//     6. What is DOM?
+    
+//     7. What is React Virtual DOM?
+    
+//     8. What does a web application or a website(composed of) have?
+
+
+// Exercises: Why React?
+//     1. Why did you chose to use react?
+    
+//     2. What measures do you use to know popularity ?
+    
+//     3. What is more popular, React or Vue ?
+
+
+// Exercises: JSX
+//     1. What is an HTML element?
+    
+//     2. How to write a self closing HTML element?
+    
+//     3. What is an HTML attribute? Write some of them
+    
+//     4. What is JSX?
+    
+//     5. What is babel?
+    
+//     6. What is a transpiler?
+
+
+// Exercises: JSX Elements
+//     1. What is a JSX element?
+    
+//     2. Write your name in a JSX element and store it in a name variable
+    
+//     3. Write a JSX element which displays your full name, country, title, gender, email, phone number. Use h1 for the name and p for the rest of the information and store it in a user variable
+    
+//     4. Write a footer JSX element
+
+
+// Exercises: Inline Style
+//     1. Create a style object for the main JSX
+    
+//     2. Create a style object for the footer and app JSX
+    
+//     3. Add more styles to the JSX elements
+
+
+// Exercises: Internal Styles
+//     1. Apply different styles to your JSX elements
+    
+
+// Exercise: Inject data to JSX
+//     1. Practice how to make JSX element and injecting dynamic data(string, number, boolean, array, object)
