53ad44bfaadcfcc718982916a7cfea315fd2c5c1
Christopher Guerrero <chrisgrr3@gmail.com>
2023-03-05 20:05:59 -0700

    Defined shoppingCart array

--- 01_Day_JavaScript_Refresher/level2_arrays.js
+++ 01_Day_JavaScript_Refresher/level2_arrays.js
@@ -16,7 +16,7 @@ console.log(count)
 
 
 // 3. In the following shopping cart add, remove, edit items
-    // const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey']
+const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey']
     // add 'Meat' in the beginning of your shopping cart if it has not been already added
     // add Sugar at the end of you shopping cart if it has not been already added
     // remove 'Honey' if you are allergic to honey
