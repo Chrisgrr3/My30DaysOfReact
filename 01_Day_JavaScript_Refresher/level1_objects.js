19e0e6c22138cf57c9772909cd6ae284ce0dcc39
Christopher Guerrero <chrisgrr3@gmail.com>
2023-03-18 23:57:25 -0700

    Added dog object properties and a method

--- 01_Day_JavaScript_Refresher/level1_objects.js
+++ 01_Day_JavaScript_Refresher/level1_objects.js
@@ -7,7 +7,14 @@ const dog = {
 console.log(dog)
 
 // 3. Add name, legs, color, age and bark properties for the dog object.The bark property is a method which return woof woof
-
+dog.name = ''
+dog.legs = 4
+dog.color = ''
+dog.age = 0
+dog.bark = function () {
+    return 'woof woof'
+}
+console.log(dog.bark())
 
 // 4. Get name, legs, color, age and bark value from the dog object
 
