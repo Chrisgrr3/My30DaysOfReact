03cfe8a3d1cf098cced6412ce577fcd4f06a3cd8
Christopher Guerrero <chrisgrr3@gmail.com>
2023-03-18 23:48:29 -0700

    Formatting

--- 01_Day_JavaScript_Refresher/level1_objects.js
+++ 01_Day_JavaScript_Refresher/level1_objects.js
@@ -1,5 +1,13 @@
-// Create an empty object called dog
-// Print the the dog object on the console
-// Add name, legs, color, age and bark properties for the dog object.The bark property is a method which return woof woof
-// Get name, legs, color, age and bark value from the dog object
-// Set new properties the dog object: breed, getDogInfo
+// 1. Create an empty object called dog
+
+
+// 2. Print the the dog object on the console
+
+
+// 3. Add name, legs, color, age and bark properties for the dog object.The bark property is a method which return woof woof
+
+
+// 4. Get name, legs, color, age and bark value from the dog object
+
+
+// 5. Set new properties the dog object: breed, getDogInfo
