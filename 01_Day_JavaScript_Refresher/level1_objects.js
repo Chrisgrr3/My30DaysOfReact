ab44d613c5a3e131be172874297da250840152ca
Christopher Guerrero <chrisgrr3@gmail.com>
2023-03-18 23:51:03 -0700

    Printed dog object to console.

--- 01_Day_JavaScript_Refresher/level1_objects.js
+++ 01_Day_JavaScript_Refresher/level1_objects.js
@@ -4,7 +4,7 @@ const dog = {
 }
 
 // 2. Print the the dog object on the console
-
+console.log(dog)
 
 // 3. Add name, legs, color, age and bark properties for the dog object.The bark property is a method which return woof woof
 
