d2e4d5013a1fa0bfbbf1f9c50ba2efd6b8522ae8
Christopher Guerrero <chrisgrr3@gmail.com>
2023-03-22 23:54:11 -0700

    Testing

--- 01_Day_JavaScript_Refresher/level1_functions.js
+++ 01_Day_JavaScript_Refresher/level1_functions.js
@@ -18,6 +18,7 @@ function areaOfCircle(r) {
     const area = Math.PI * r * r
     return area
 }
+console.log(areaOfCircle(2))
 
 // 4. Temperature in oC can be converted to oF using this formula: oF = (oC x 9 / 5) + 32. Write a function which convert oC to oF convertCelciusToFahrenheit.
 
