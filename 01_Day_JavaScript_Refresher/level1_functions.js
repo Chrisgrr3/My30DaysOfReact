c97501b59541c1ba83997fc3b426b71b94bcbca2
Christopher Guerrero <chrisgrr3@gmail.com>
2023-03-24 23:08:55 -0700

    Created skeleton for bmi calculation function and initialized parameters

--- 01_Day_JavaScript_Refresher/level1_functions.js
+++ 01_Day_JavaScript_Refresher/level1_functions.js
@@ -36,11 +36,13 @@ console.log(convertCelciusToFahrenheit(100))
 
 // 5. Body mass index(BMI) is calculated as follows: bmi = weight in Kg / (height x height) in m2.Write a function which calculates bmi.BMI is used to broadly define different weight groups in adults 20 years old or older.Check if a person is underweight, normal, overweight or obese based the information given below.
 
-    // The same groups apply to both men and women.
-    // Underweight: BMI is less than 18.5
-    // Normal weight: BMI is 18.5 to 24.9
-    // Overweight: BMI is 25 to 29.9
-    // Obese: BMI is 30 or more
+// The same groups apply to both men and women.
+// Underweight: BMI is less than 18.5
+// Normal weight: BMI is 18.5 to 24.9
+// Overweight: BMI is 25 to 29.9
+// Obese: BMI is 30 or more
+function calculateBMI(weight, height) {
 
+}
 
 // 6. Write a function called checkSeason, it takes a month parameter and returns the season: Autumn, Winter, Spring or Summer.
