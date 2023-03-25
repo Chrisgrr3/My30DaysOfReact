9df999c36e71d0dfd77c95b9232bccf66e50222b
Christopher Guerrero <chrisgrr3@gmail.com>
2023-03-24 23:19:37 -0700

    Created underweight condition for bmi function

--- 01_Day_JavaScript_Refresher/level1_functions.js
+++ 01_Day_JavaScript_Refresher/level1_functions.js
@@ -43,6 +43,10 @@ console.log(convertCelciusToFahrenheit(100))
 // Obese: BMI is 30 or more
 function calculateBMI(weight, height) {
     const bmi = weight / (Math.pow(height, 2))
+
+    if (bmi < 18.5) {
+        return 'Underweight'
+    }
 }
 
 // 6. Write a function called checkSeason, it takes a month parameter and returns the season: Autumn, Winter, Spring or Summer.
