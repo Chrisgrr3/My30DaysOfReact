52379bff7b2d80a44f2dad769ab000eea295aa10
Christopher Guerrero <chrisgrr3@gmail.com>
2023-03-24 23:01:05 -0700

    Created skeleton for temperature conversion function

--- 01_Day_JavaScript_Refresher/level1_functions.js
+++ 01_Day_JavaScript_Refresher/level1_functions.js
@@ -26,7 +26,9 @@ console.log(areaOfCircle(1))
 console.log(areaOfCircle(-1))
 
 // 4. Temperature in oC can be converted to oF using this formula: oF = (oC x 9 / 5) + 32. Write a function which convert oC to oF convertCelciusToFahrenheit.
+function convertCelciusToFahrenheit(c) {
 
+}
 
 // 5. Body mass index(BMI) is calculated as follows: bmi = weight in Kg / (height x height) in m2.Write a function which calculates bmi.BMI is used to broadly define different weight groups in adults 20 years old or older.Check if a person is underweight, normal, overweight or obese based the information given below.
 
