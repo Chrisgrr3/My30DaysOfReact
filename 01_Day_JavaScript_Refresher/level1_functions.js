415f11be10fa1a9640b3426ed8a5f7528deeb3db
Christopher Guerrero <chrisgrr3@gmail.com>
2023-03-25 21:33:08 -0700

    Prevented negative weight input

--- 01_Day_JavaScript_Refresher/level1_functions.js
+++ 01_Day_JavaScript_Refresher/level1_functions.js
@@ -47,6 +47,9 @@ function calculateBMI(weight, height) {
     if (weight < 0) {
         return 'You did not enter a valid weight.'
     }
+    if (height < 0) {
+        return 'You did not enter a valid height.'
+    }
     if (bmi < 18.5) {
         return `BMI: ${bmi}, Underweight`
     } else if (bmi < 24.9) {
@@ -59,6 +62,6 @@ function calculateBMI(weight, height) {
 }
 console.log(calculateBMI(55, 1.3))
 console.log(calculateBMI(-4, 1))
-console.log(calculateBMI(55, -2))
+console.log(calculateBMI(55, -1))
 
 // 6. Write a function called checkSeason, it takes a month parameter and returns the season: Autumn, Winter, Spring or Summer.
