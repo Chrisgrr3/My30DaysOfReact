635c1c444a0418a76a7c6066ded4194da16d1838
Christopher Guerrero <chrisgrr3@gmail.com>
2023-03-25 23:11:26 -0700

    Testing

--- 01_Day_JavaScript_Refresher/level1_functions.js
+++ 01_Day_JavaScript_Refresher/level1_functions.js
@@ -36,6 +36,7 @@ function convertCelciusToFahrenheit(celsius) {
 }
 console.log(convertCelciusToFahrenheit(0))
 console.log(convertCelciusToFahrenheit(100))
+console.log(convertCelciusToFahrenheit('hi'))
 
 // 5. Body mass index(BMI) is calculated as follows: bmi = weight in Kg / (height x height) in m2.Write a function which calculates bmi.BMI is used to broadly define different weight groups in adults 20 years old or older.Check if a person is underweight, normal, overweight or obese based the information given below.
 
