c5fc71c9c8d5e72afb1fd380c386192edf67008b
Christopher Guerrero <chrisgrr3@gmail.com>
2023-04-01 23:29:49 -0700

    Created skeleton for userIdGenerator function

--- 01_Day_JavaScript_Refresher/level2_functions.js
+++ 01_Day_JavaScript_Refresher/level2_functions.js
@@ -80,3 +80,6 @@ function sumOfArgs(...args) {
 }
 
 // 11. Declare a function name userIdGenerator.When this function is called it generates seven character id.The function return the id.
+function userIdGenerator() {
+
+}
