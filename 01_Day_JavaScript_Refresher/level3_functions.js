dea36076fe3920c7a0832c1a6e71a08c1eebc5c5
Christopher Guerrero <chrisgrr3@gmail.com>
2023-04-02 23:38:41 -0700

    Formatting

--- 01_Day_JavaScript_Refresher/level3_functions.js
+++ 01_Day_JavaScript_Refresher/level3_functions.js
@@ -1,29 +1,17 @@
-// Declare a function name userIdGeneratedByUser.It doesn’t take any parameter but it takes two inputs using prompt().One of the input is the number of characters and the second input is the number of ids which are supposed to be generated.
+// 1. Declare a function name userIdGeneratedByUser.It doesn’t take any parameter but it takes two inputs using prompt().One of the input is the number of characters and the second input is the number of ids which are supposed to be generated.
 
-//     userIdGeneratedByUser()
-// 'kcsy2
-// SMFYb
-// bWmeq
-// ZXOYh
-// 2Rgxf
-// '
-// userIdGeneratedByUser()
-// '1GCSgPLMaBAVQZ26
-// YD7eFwNQKNs7qXaT
-// ycArC5yrRupyG00S
-// UbGxOFI7UXSWAyKN
-// dIV0SSUTgAdKwStr
-// '
-// Write a function generateColors which can generate any number of hexa or rgb colors.
 
-//     console.log(generateColors('hexa', 3)) // ['#a3e12f', '#03ed55', '#eb3d2b']
-// console.log(generateColors('hexa', 1)) // '#b334ef'
-// console.log(generateColors('rgb', 3)) // ['rgb(5, 55, 175)', 'rgb(50, 105, 100)', 'rgb(15, 26, 80)']
-// console.log(generateColors('rgb', 1)) // 'rgb(33,79, 176)'
-// Call your function shuffleArray, it takes an array as a parameter and it returns a shuffled array
+// 2. Write a function generateColors which can generate any number of hexa or rgb colors.
 
-// Call your function factorial, it takes a whole number as a parameter and it return a factorial of the number
 
-// Call your function isEmpty, it takes a parameter and it checks if it is empty or not
+// 3. Call your function shuffleArray, it takes an array as a parameter and it returns a shuffled array
 
-// Write a function called average, it takes an array parameter and returns the average of the items.Check if all the array items are number types.If not give return reasonable feedback.
+
+// 4. Call your function factorial, it takes a whole number as a parameter and it return a factorial of the number
+
+
+// 5. Call your function isEmpty, it takes a parameter and it checks if it is empty or not
+
+
+// 6. Write a function called average, it takes an array parameter and returns the average of the items.Check if all the array items are number types.If not give return reasonable feedback.
+
